6004c42ab83547d8a6c55c333452950cebd85ab8
Mahasak Pijittum <mahasak@gmail.com>
2018-02-03 23:46:03 +0700

    change to requestify

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 
-var axios = require('axios');
+var requestify = require('requestify');
 var cheerio = require('cheerio');
-const svgson = require('svgson')
+var svgson = require('svgson')
 var { DateTime } = require('luxon');
 
-var getGitHubContributes = function(githubUsername,  server = 'github.com') {
+var getGitHubContributes = async (githubUsername,  server = 'github.com') => {
   var endpoint = 'https://' + server + '/users/' + githubUsername +'/contributions?to=' + DateTime.local().toISODate();
-
-  axios.get(endpoint + githubUsername)
-    .then(response => {
-      const $ = cheerio.load(response.data);
-      $('.day').each((_, e) => {
+  var data = [];
+  var response = await requestify.get(endpoint);
+  var $ = cheerio.load(response.getBody());
+  $('.day').each((_, e) => {
         date = $(e).data('date');
         count = parseInt($(e).data('count'), 10);
-        console.log(date + ': '+count)
+        data.push({date, count});
       });
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  return data;
 }
 
-getGitHubContributes('mahasak');
+module.exports = getGitHubContributes;
