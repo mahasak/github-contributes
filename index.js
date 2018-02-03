1840268c08846c1ddab22fa30937dbe887a95805
Mahasak Pijittum <mahasak@gmail.com>
2018-02-03 23:29:17 +0700

    change to use class .day for retrieval

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 
-var endpoint = 'https://github.com/users/mahasak/contributions?to=2018-02-02';
 var axios = require('axios');
 var cheerio = require('cheerio');
 const svgson = require('svgson')
+var { DateTime } = require('luxon');
 
-var getGitHubContributes = function(githubUsername) {
+var getGitHubContributes = function(githubUsername,  server = 'github.com') {
+  var endpoint = 'https://' + server + '/users/' + githubUsername +'/contributions?to=' + DateTime.local().toISODate();
+
   axios.get(endpoint + githubUsername)
-  .then(response => {
-    const $ = cheerio.load(response);
-    var contributions = 0;
-    console.log(response);
-
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .then(response => {
+      const $ = cheerio.load(response.data);
+      $('.day').each((_, e) => {
+        date = $(e).data('date');
+        count = parseInt($(e).data('count'), 10);
+        console.log(date + ': '+count)
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
-
 
 getGitHubContributes('mahasak');
